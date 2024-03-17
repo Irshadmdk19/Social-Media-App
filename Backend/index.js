74c473731721fbe107c0adc13e225d804116ea21
Muhammad Irshad <85778819+Irshadmdk19@users.noreply.github.com>
2024-03-17 12:02:53 +0530

    added cors and cookie-parser middlewares

--- Backend/index.js
+++ Backend/index.js
@@ -5,11 +5,15 @@ import authRoutes from "./routes/auth.js"
 import commentsRoutes from "./routes/comments.js"
 import likesRoutes from "./routes/likes.js"
 
+import cors from "cors"
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 // Middlewares 
 app.use(express.json()) // Parse incoming requests with JSON payloads
+app.use(cors());
+app.use(cookieParser());
 
 app.use("/api/auth",authRoutes)
 app.use("/api/users",userRoutes)
